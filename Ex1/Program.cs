a7657c84d8675e305e091605f96382718a826b32
s00187096 <55234394+s00187096@users.noreply.github.com>
2019-09-30 12:36:28 +0100

    Completed Exercise 1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -10,7 +10,21 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
+            Car c1 = new Car();
+            c1.Make = "Ford";
+            c1.Model = "Focus";
+            c1.CurrentSpeed = 0;
+            c1.EngineSize = 1.6;
 
+            Car c2 = new Car();
+            c2.Make = "Opel";
+            c2.Model = "Astra";
+            c2.CurrentSpeed = 0;
+            c2.EngineSize = 1.2;
+
+            Console.WriteLine($"the {c1.Make}{c1.Model}, engine size {c1.EngineSize}L is Currently travelling at {c1.CurrentSpeed} km/h");
+
+            Console.WriteLine($"the {c2.Make}{c2.Model}, engine size {c2.EngineSize}L is Currently travelling at {c2.CurrentSpeed} km/h");
         }
     }
 }
